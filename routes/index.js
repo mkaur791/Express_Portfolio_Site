ced869c5a9ab1f1b8fc2f7576ebefbed956acf20
Manvir Kaur <mkaur791@my.centennialcollege.ca>
2021-10-20 03:33:36 -0400

    created pages and added content to pages

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home',page: 'home' });
 });
 
 /* GET Home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home',page: 'home' });
 });
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About',page: 'about' });
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Projects', page: 'projects' });
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Services' , page: 'services'});
 });
 
 /* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Contact' , page: 'contact' });
 });
 
 module.exports = router;
